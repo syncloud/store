218399e315b9ebc90b79d944a4463d01946c39fa
Boris Rybalkin <ribalkin@gmail.com>
2023-06-30 12:21:05 +0100

    fix snapd1

--- test/test.sh
+++ test/test.sh
@@ -27,9 +27,7 @@ mkdir -p $LOG_DIR
 $SCP ${DIR}/install-store.sh root@$DEVICE:/install-store.sh
 $SCP ${DIR}/../out/syncloud-store-*.tar.gz root@$DEVICE:/syncloud-store.tar.gz
 
-
-VERSION=$(curl http://apps.syncloud.org/releases/stable/snapd.version)
-wget --progress=dot:giga http://apps.syncloud.org/apps/snapd-${VERSION}-amd64.tar.gz -O snapd1.tar.gz
+wget --progress=dot:giga https://github.com/syncloud/snapd/releases/download/2/snapd-361-amd64.tar.gz -O snapd2.tar.gz
 $SCP snapd1.tar.gz root@$DEVICE:/
 wget --progress=dot:giga https://github.com/syncloud/snapd/releases/download/3-rc2/snapd-510-amd64.tar.gz -O snapd2.tar.gz
 $SCP snapd2.tar.gz root@$DEVICE:/
