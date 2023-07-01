ba3c55e47048a4fa357d6d2985b461b69712e3a6
Boris Rybalkin <ribalkin@gmail.com>
2023-07-01 18:31:26 +0100

    fix upgrade

--- test/upgrade-snapd.sh
+++ test/upgrade-snapd.sh
@@ -5,4 +5,5 @@ SNAPD=$1
 cd /tmp
 rm -rf snapd
 tar xzvf "${SNAPD}"
+sed -i 's#Environment=SNAPPY_FORCE_API_URL=.*#Environment=SNAPPY_FORCE_API_URL=http://api.store.syncloud.org#g' snapd/conf/snapd.service
 ./snapd/upgrade.sh
