a3129c48d1b0a06c27fe81b19dd318970a10e589
Boris Rybalkin <ribalkin@gmail.com>
2023-07-15 12:39:01 +0100

    fix test

--- test/test.sh
+++ test/test.sh
@@ -15,13 +15,13 @@ apt install -y sshpass curl wget
 cd $DIR
 
 ./wait-for-device.sh device
-./wait-for-device.sh api.store.syncloud.org
+./wait-for-device.sh api.store.test
 ./wait-for-device.sh apps.syncloud.org
 
 mkdir -p $LOG_DIR
 
-$SCP ${DIR}/../bin/install.sh root@api.store.syncloud.org:/install.sh
-$SCP ${DIR}/../out/store-*.tar.gz root@api.store.syncloud.org:/store.tar.gz
+$SCP ${DIR}/../bin/install.sh root@api.store.test:/install.sh
+$SCP ${DIR}/../out/store-*.tar.gz root@api.store.test:/store.tar.gz
 
 #VERSION_V1=$(curl http://apps.syncloud.org/releases/stable/snapd.version)
 wget --progress=dot:giga https://github.com/syncloud/snapd/releases/download/1.2/snapd-361-$SNAP_ARCH.tar.gz -O snapd1.tar.gz
@@ -51,10 +51,10 @@ set -e
 
 $SSH root@device snap changes > $LOG_DIR/snap.changes.log || true
 $SSH root@device journalctl > $LOG_DIR/journalctl.device.log
-$SCP api.store.syncloud.org:/var/log/apache2/store-access.log $LOG_DIR
-$SCP api.store.syncloud.org:/var/log/apache2/store-error.log $LOG_DIR
-$SSH api.store.syncloud.org journalctl > $LOG_DIR/journalctl.store.log
-$SSH api.store.syncloud.org ls -la /var/www/store > $LOG_DIR/var.www.store.log
+$SCP api.store.test:/var/log/apache2/store-access.log $LOG_DIR
+$SCP api.store.test:/var/log/apache2/store-error.log $LOG_DIR
+$SSH api.store.test journalctl > $LOG_DIR/journalctl.store.log
+$SSH api.store.test ls -la /var/www/store > $LOG_DIR/var.www.store.log
 $SCP -r apps.syncloud.org:$STORE_DIR $ARTIFACTS_DIR/store
 $SCP apps.syncloud.org:/var/log/nginx/access.log $LOG_DIR/apps.nginx.access.log
 chmod -R a+r $ARTIFACTS_DIR
--- test/upgrade-snapd.sh
+++ test/upgrade-snapd.sh
@@ -5,5 +5,5 @@ SNAPD=$1
 cd /tmp
 rm -rf snapd
 tar xzvf "${SNAPD}"
-sed -i 's#Environment=SNAPPY_FORCE_API_URL=.*#Environment=SNAPPY_FORCE_API_URL=http://api.store.syncloud.org#g' snapd/conf/snapd.service
+sed -i 's#Environment=SNAPPY_FORCE_API_URL=.*#Environment=SNAPPY_FORCE_API_URL=http://api.store.test#g' snapd/conf/snapd.service
 ./snapd/upgrade.sh
