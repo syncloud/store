1b4629f757809d6398b5e2f25069bea45106c659
Boris Rybalkin <ribalkin@gmail.com>
2023-07-16 20:12:19 +0100

    snapd v2.575

--- test/publish.sh
+++ test/publish.sh
@@ -17,26 +17,7 @@ apt update
 apt install -y sshpass curl
 cd $DIR
 
-function wait_for_host() {
-  local host=$1
-  attempts=100
-  attempt=0
-  set +e
-  sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@$host date
-  while test $? -gt 0
-  do
-    if [ $attempt -gt $attempts ]; then
-      exit 1
-    fi
-    sleep 3
-    echo "Waiting for SSH $attempt"
-    attempt=$((attempt+1))
-    sshpass -p syncloud ssh -o StrictHostKeyChecking=no root@$host date
-  done
-  set -e
-}
-
-wait_for_host apps.syncloud.org
+./wait-for-device.sh apps.syncloud.org
 
 mkdir -p $LOG_DIR
 STORE_DIR=/var/www/html
