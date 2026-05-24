876753932ba81c94d9771faeeedf56664ede5732
Boris Rybalkin <ribalkin@gmail.com>
2026-05-25 00:44:31 +0100

    install-snapd-v2.sh: point SNAPPY_FORCE_API_URL at api.store (not api.store.test)
    The device service's snapd was still configured to query the old
    hostname. TestUnknown's 'snap install unknown' resolved api.store.test
    to nothing and failed with a DNS error instead of the expected 'not
    found'.

--- test/install-snapd-v2.sh
+++ test/install-snapd-v2.sh
@@ -5,5 +5,5 @@ SNAPD=$1
 cd /tmp
 rm -rf snapd
 tar xzvf "${SNAPD}"
-sed -i 's#Environment=SNAPPY_FORCE_API_URL=.*#Environment=SNAPPY_FORCE_API_URL=http://api.store.test#g' snapd/conf/snapd.service
+sed -i 's#Environment=SNAPPY_FORCE_API_URL=.*#Environment=SNAPPY_FORCE_API_URL=http://api.store#g' snapd/conf/snapd.service
 ./snapd/install.sh
