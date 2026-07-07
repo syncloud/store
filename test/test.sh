1359899319d4c707496664e7634cc92140fdd1fe
Boris Rybalkin <ribalkin@gmail.com>
2026-07-07 09:51:46 +0100

    test: fetch hack-free snapd from github release + verify download
    The artifact server is IPv6-only and unreachable from the CI container
    (ci.syncloud.org resolves to an IPv4 box that refuses :8081), so the
    earlier wget silently failed and the e2e ran against the device's
    pre-installed snapd. Use the github pre-release (IPv4) and add 'gzip -t'
    so a failed download hard-fails the test.

--- test/test.sh
+++ test/test.sh
@@ -15,7 +15,8 @@ mkdir -p $LOG_DIR
 cd $DIR
 ./wait-for-device.sh device
 
-wget --progress=dot:giga http://ci.syncloud.org:8081/files/snapd/655-${SNAP_ARCH}/snapd-655-${SNAP_ARCH}.tar.gz -O snapd2.tar.gz
+wget --progress=dot:giga https://github.com/syncloud/snapd/releases/download/snapd-655-hackfree-test/snapd-655-${SNAP_ARCH}.tar.gz -O snapd2.tar.gz
+gzip -t snapd2.tar.gz
 $SCP snapd2.tar.gz root@device:/
 $SCP ${DIR}/install-snapd-v2.sh root@device:/
 $SCP ${DIR}/testapp2/testapp2_1_$SNAP_ARCH.snap root@device:/testapp2_1.snap
